cfbf91e17f3aaa68690450d6a2db3ea506d79dbf
DJ Hersh <DanielJHersh@gmail.com>
2020-12-14 11:18:32 -0800

    Created an express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+});

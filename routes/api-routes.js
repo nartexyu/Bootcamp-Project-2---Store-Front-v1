d5821cfd90695a3164e01dc8fe6f8c4a48a9b3c8
DJ Hersh <DanielJHersh@gmail.com>
2020-12-15 11:03:40 -0800

    committing my current work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ const db = require("../models");
 const passport = require("../config/passport");
 const { resolve } = require("path");
 const { uploader, cloudinaryConfig } = require("../config/middleware/cloudinaryConfig");
-const { multerUploads, dataUri } = require("../config/middleware/multer");
+const { multerUploads, getContent } = require("../config/middleware/multer");
 
 module.exports = function(app) {
 
@@ -42,10 +42,13 @@ module.exports = function(app) {
 
     app.use("*", cloudinaryConfig);
 
-    app.post("/upload", multerUploads, (req, res) => {
+    app.post("/upload", multerUploads, async (req, res) => {
+      console.log("req.file :", req.file);
+      // console.log(getContent(req));
       if (req.file) {
-        const file = dataUri(req).content;
-        return uploader.upload(file).then(result => {
+        const file = await getContent(req);
+        console.log(file);
+        return uploader.upload(req.file.path).then(result => {
           const image = result.url;
           return res.status(200).json({
             message: "Your image has been uploaded successfully",
@@ -63,21 +66,22 @@ module.exports = function(app) {
     });
 
     app.post("/api/uploadimage", (req, res) => {
+      console.log(req.files);
       const data = {
         image: req.body.image,
       };
-      uploader.upload(data.image).then(result => {
-        res.status(200).send({
-          message: "success",
-          result
-        });
-        console.log(result.url);
-      }).catch(error => {
-        res.status(500).send({
-          message: "failure",
-          error
-        });
-      });
+      // uploader.upload(req.files.image.path).then(result => {
+      //   res.status(200).send({
+      //     message: "success",
+      //     result
+      //   });
+      //   console.log(result.url);
+      // }).catch(error => {
+      //   res.status(500).send({
+      //     message: "failure",
+      //     error
+      //   });
+      // });
     });
     // API Routes go here
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = app => {
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index/index.html"));
+        res.sendFile(path.join(__dirname, "../test.html"));
+        // res.sendFile(path.join(__dirname, "../public/index/index.html"));
     });
 }

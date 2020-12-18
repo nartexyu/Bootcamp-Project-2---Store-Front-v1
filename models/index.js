1e8dfb1e01c210ae7aec829b5ae6569371fbeb67
DJ Hersh <DanielJHersh@gmail.com>
2020-12-18 01:56:58 -0800

    changes

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path      = require('path');
 const Sequelize = require('sequelize');
 const basename  = path.basename(module.filename);
 let env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/../config/config.json')[env];
+const config    = require(__dirname + '/../config/config.js')[env];
 let db          = {};
 
 if (config.use_env_variable) {
